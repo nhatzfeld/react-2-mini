75544f9a0332d7569c0593b4958b65c369d6d900
Nathan <n.hatz@yahoo.com>
2018-04-18 23:21:48 -0500

    on step 4

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Components
-import EditToggle from './components/EditToggle';
-import ColorChanger from './components/ColorChanger';
-import SizeChanger from './components/SizeChanger';
-import FamilyChanger from './components/FamilyChanger';
-import TextContainer from './components/TextContainer';
+import EditToggle from "./components/EditToggle";
+import ColorChanger from "./components/ColorChanger";
+import SizeChanger from "./components/SizeChanger";
+import FamilyChanger from "./components/FamilyChanger";
+import TextContainer from "./components/TextContainer";
 
 class App extends Component {
-  // constructor
+  constructor() {
+    super();
+    this.state = {
+      fontColor: "black",
+      fontSize: 12,
+      fontFamily: "monospace",
+      allowEdit: true
+    };
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updateEditStatus = this.updateFamily.bind(this);
+  }
 
-  // updateColor
+  updateColor(val) {
+    this.setState({ fontColor: val });
+  }
 
-  // updateSize
+  updateSize(val) {
+    this.setState({ fontColor: val });
+  }
 
-  // updateFamily
+  updateFamily(val) {
+    this.setState({ fontFamily: val });
+  }
 
-  // updateEditStatus
+  updateEditStatus(val) {
+    this.setState({ allowEdit: val });
+  }
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          <EditToggle />
+          <ColorChanger />
+          <SizeChanger />
+          <FamilyChanger />
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+          <TextContainer />
         </div>
       </div>
-    )
+    );
   }
 }
 
